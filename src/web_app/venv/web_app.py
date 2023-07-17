3fc85ad3b00fd8bd4418c505ed8a1009a1ca8c04
Kirill <k007pm@yandex.ru>
2023-07-17 16:01:03 +0500

    clean index

--- src/web_app/venv/web_app.py
+++ src/web_app/venv/web_app.py
@@ -4,17 +4,11 @@ from ticers import Ticers
 import pandas as pd
 
 app = Flask(__name__)
-t = Ticers()
+#t = Ticers()
 
 @app.route("/")
 def chart():
-    t = Ticers().dowload_data('GAZP', '2020-01-01', '2022-12-13')
-    p = pd.read_csv('./data/company/GAZP.csv')    
-    legend = 'Monthly Data'
-    labels = p['Date']
-    values = p['GAZP']
-    return render_template('index.html', values=values, labels=labels, legend=legend)
+    return render_template('index.html')
 
 if __name__ == "__main__": 
-
     app.run('127.0.0.1', port=5001 ,debug=True)
